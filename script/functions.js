b183da41aa46532c7882ff76f541e2eeb94430a8
Bin Giulio Liu <bingiulio.liu@gmail.com>
2026-03-31 17:21:20 +0200

    DONE da ottimizzare

--- script/functions.js
+++ script/functions.js
@@ -1,3 +1,4 @@
+'use strict';
 // PALINDROMA
 // la parola scritta nel prompt va convertita o tutto in maiuscola o in minuscola
 // inverto l`input con .split('').reverse().join(''): arrayzzo, inverto e riunisco
@@ -21,10 +22,39 @@ function checkPalindromo(testo) {
 let sceltaGiocatore = prompt('PARI o DISPARI?');
 
 if (sceltaGiocatore === 'PARI' || sceltaGiocatore === 'DISPARI') {
-    console.log(`Hai scelto ${sceltaGiocatore}`)
+    console.log(`Hai scelto: ${sceltaGiocatore}`)
 }
     else{
-        console.log(`Riprova, inserisci un valore corretto`)
+        console.log(`Riprova, inserisci PARI o DISPARI`)
     }
 
-let numeroGiocatore = prompt('Scegli un numero');
+let numeroGiocatore = parseInt(prompt('Scegli un numero da 1 a 5'));
+if (isNaN(numeroGiocatore) || numeroGiocatore < 1 || numeroGiocatore > 5) {
+    console.log(`Riprova, inserisci un valore corretto`)
+}
+    else 
+        console.log(`Hai scelto ${numeroGiocatore}`);
+
+function letsPlay() {
+    let numeroPc = Math.floor(Math.random() * 5) + 1;
+    return numeroPc;
+}
+let numeroRandomPc = letsPlay();
+console.log(`PC ha scelto: ${numeroRandomPc}`);
+let somma = (numeroGiocatore) + (numeroRandomPc);
+console.log(somma)
+function checkPariDispari(num) {
+    if (num % 2 === 0) {
+        return 'PARI';
+    }   else {
+        return 'DISPARI';
+    }
+}
+
+let checkRisultato = checkPariDispari(somma);
+
+if (checkRisultato === sceltaGiocatore) {
+    console.log(`Totale: ${somma}. Hai vinto!`);
+}   else {
+    console.log(`Totale: ${somma}. Hai perso!`);
+}
